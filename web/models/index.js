1506eced03ca1f2429a77f2a24662d12a6bb662b
TokinRing <support@gnetsolutions.net>
2019-01-09 23:53:30 -0800

    Fix model index.js

--- web/models/index.js
+++ web/models/index.js
@@ -17,32 +17,28 @@ let modules = [
   require('./user.js')
 ];
 
-function getModels(config, force = false) {
-  // Initialize sequelize DataType
-  let sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {host: config.db.host, dialect: config.db.dialect});
+// Initialize sequelize DataType
+let sequelize = new Sequelize(config.db.database, config.db.username, config.db.password, {
+  host: config.db.host,
+  dialect: config.db.dialect
+});
 
-  // Sequelize each of the model modules
-  modules.forEach((module) => {
-    let model = module(sequelize, Sequelize, config);
-    models[model.name] = model;
-  });
+// Sequelize each of the model modules
+modules.forEach((module) => {
+  let model = module(sequelize, Sequelize, config);
+  models[model.name] = model;
+});
 
-  // Initialize model object relation mapping
-  Object.keys(models).forEach(function(modelName) {
-    if (models[modelName].associate) {
-      models[modelName].associate(models);
-    }
-  });
+// Initialize model object relation mapping
+Object.keys(models).forEach(function(modelName) {
+  if (models[modelName].associate) {
+    models[modelName].associate(models);
+  }
+});
 
-  // Store instance and DataType
-  models.sequelize = sequelize;
-  models.Sequelize = Sequelize;
+// Store instance and DataType
+models.sequelize = sequelize;
+models.Sequelize = Sequelize;
 
-  // Return object of all sequelized models
-  return models
-}
-
-
-module.exports = {
-  getModels
-};
+// Export object containting models
+module.exports = models
